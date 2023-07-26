e175e81a12b571816775675ff724fac7d9a46543
Rafael Rossi <rtrafaeltrova@gmail.com>
2023-07-25 23:04:39 -0300

    1 - Navegacao entre paginas - 1 - Basicos: Link, Head e Script

---
+++ nextjs-blog/pages/posts/first-post.js
@@ -0,0 +1,32 @@
+import Link from "next/link";
+
+import Image from "next/image";
+import Head from "next/head";
+import Script from "next/script";
+
+export default function FirstPost() {
+    return (
+        <>
+            { /* "Head" => Componente do ReactJs, usado para alterar facilmente o head da pagina sem necessitar de reescrever a maior parte do codigo  */ }
+            <Head>
+                <title>First Post</title>
+
+                { /* "Script" => Componente do NextJs, otimiza e executa quando scripts sao requisitados e executados */ }
+                <Script
+                    src="https://connect.facebook.net/en_US/sdk.js"
+                    strategy="lazyOnload" /* "strategy" => Controla quando o script de terceiros deve ser carregado */
+                    onLoad={() => { /* "onLoad" => Usado para executar codigo JS imediatamente apos o script terminar de carregar */
+                        console.log("Script carregado com sucesso, window.FB esta disponivel")
+                    }}
+                />
+            </Head>
+
+            <h1>First Post</h1>
+
+            <h2>
+                { /* "Link" => Componente do NextJs, usado para alternar entre paginas ou rotas da aplicacao */ }
+                <Link href="/">Back to home</Link>
+            </h2>
+        </>
+    );
+}
